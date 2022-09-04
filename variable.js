cf4eb18e4667fd27ea6c2901bd0a38ed09ed113e
shohel-ranaC <akashshohel422@gmail.com>
2022-09-04 17:56:12 +0600

    first time js learning

---
+++ variable.js
@@ -0,0 +1,21 @@
+var name = "amr sonar bangla ami tumay valobasi";
+
+var num1 = 100;
+var num2 = 100;
+var num3 = "100";
+
+var total = num1 + num3;
+console.log(total);
+
+console.log(name.indexOf('ami'));
+console.log(name.indexOf('v'));
+
+console.log(name.split('sonar'));
+console.log(name.split(' '));
+
+var num4 = .1;
+var num5 = .2;
+var total = num4 + num5;
+console.log(total.toFixed(2));
+console.log(total.toFixed(1));
+
